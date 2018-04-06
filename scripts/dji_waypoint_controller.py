9bdcc7394a8ce39372be5993f86ac5887cdd2b39
javieryu <javiercyu@gmail.com>
2018-04-06 14:13:55 -0700

    fixed the pid controller

--- scripts/dji_waypoint_controller.py
+++ scripts/dji_waypoint_controller.py
@@ -43,7 +43,7 @@ class PID_Controller(object):
   carrot = State(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0) # x,y,z,t,xs,ys,zs,ts
   goal = State(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0) # x,y,z,t,xs,ys,zs,ts
   
-  pid_global_gains =   State( 0.25, 0.25,  0.5, 200.0, 0.0, 0.0, 0.1, 10.0)
+  pid_global_gains =   State( 0.25, 0.25,  0.5, 200.0, 0.01, 0.01, 0.1, 10.0)
   sum_err = State(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
   max_state = State( 200.0, 200.0,40.0, 50.0, 0.1, 0.1, 1.0,100.0)
   min_state = State(-200.0,-200.0, 2.0,-50.0,-0.1,-0.1,-1.0,-100.0)
@@ -59,6 +59,7 @@ class PID_Controller(object):
 
   goal_tolerance = 1.0
   control_radius = 2.5
+  carrot_distance = 5.0
 
   def init(self, drone, ai, aa, cs):
     
@@ -77,7 +78,7 @@ class PID_Controller(object):
     self.cruising_speed = cs
     self.index = ai
     # Setup publisher
-    self.pub_twist = rospy.Publisher('/uav' + str(self.index) + '/cmd_vel', Twist, queue_size=10)
+    self.pub_twist = rospy.Publisher('/uav' + str(self.index) + '/cmd_vel', Twist, queue_size=1)
     self.pub_marker = rospy.Publisher('/PID_Control/path_markers', Marker, queue_size=1)
     # Setup subscriber
     self.sub_odom = rospy.Subscriber('/global/odom', Odometry, self.odom_callback,  queue_size=1 )
@@ -99,17 +100,29 @@ class PID_Controller(object):
     self.map_num = -1
 
     # basically used to see how long ago the path was updated
-    self.path_valid_duration = rospy.Duration(2.0)
+    self.path_valid_duration = rospy.Duration(10.0)
     self.path_update_time = rospy.Time(0.0)
 
     # main control loop that outputs commands to the DJI quad
     self.control_timer = rospy.Timer(rospy.Duration(0.25), self.control_timer_callback)
     # calls the costmap planner and requests a path
-    self.path_timer = rospy.Timer(rospy.Duration(0.5), self.path_timer_callback)
+    self.path_timer = rospy.Timer(rospy.Duration(5.0), self.path_timer_callback)
     # updates the carrot
     self.carrot_timer = rospy.Timer(rospy.Duration(0.1), self.carrot_timer_callback)
     # activates the quad
     self.activation_timer = rospy.Timer(rospy.Duration(10.0), self.activation_timer_callback)
+    # status msg timer
+    self.status_msg_timer = rospy.Timer(rospy.Duration(1.0), self.status_msg_timer_callback)
+
+  def status_msg_timer_callback(self, event):
+    if not self.good_carrot:
+        rospy.logwarn("PID Controller:: bad carrot")
+    if not self.path_initialized:
+        rospy.logwarn("PID Controller:: path is not initialized")
+    if not self.initialized:
+        rospy.logwarn("PID Controller:: I am not initialized")
+    if not self.goal_initialized:
+      rospy.logwarn("PID_Controller::goal is not initialized")
 
   def activation_timer_callback(self, event):
     self.drone.request_sdk_permission_control() #Request to obtain control
@@ -124,29 +137,32 @@ class PID_Controller(object):
         self.good_carrot = self.update_carrot()
 
   def path_timer_callback(self, event):
+    #rospy.logwarn("PID Controller:: path callback: requesting path")
     self.request_path()
 
   def request_path(self):
-    # this updates the path using the costmap
-    if self.use_naive_pid:
-      self.path = []
-    elif self.use_a_star_path:
-      start = PoseStamped()
-      start.pose.position.x = self.cLoc.x
-      start.pose.position.y = self.cLoc.y
-      goal = PoseStamped()
-      goal.pose.position.x = self.goal.x
-      goal.pose.position.y = self.goal.y
-      path_out = Path()
-      path_out.poses.append(start)
-      path_out.poses.append(goal)
-      self.pub_path_request.publish(path_out)
-    else:
-      rospy.logerr("PID_Controller::request_path: NO PLANNING METHOD PROVIDED")
-      self.goal_initialized = False
-    
-    #for i, p in enumerate(self.path):
-    #  self.make_rviz_marker(p, i, 1.0,0.0,0.0,0.5,10.0)
+    if self.initialized:
+        #rospy.logwarn("PID Controller:: request_path: in")
+        # this updates the path using the costmap
+        if self.use_naive_pid:
+          self.path = []
+        elif self.use_a_star_path:
+          start = PoseStamped()
+          start.pose.position.x = self.cLoc.x
+          start.pose.position.y = self.cLoc.y
+          goal = PoseStamped()
+          goal.pose.position.x = self.goal.x
+          goal.pose.position.y = self.goal.y
+          path_out = Path()
+          path_out.poses.append(start)
+          path_out.poses.append(goal)
+          self.pub_path_request.publish(path_out)
+        else:
+          rospy.logerr("PID_Controller::request_path: NO PLANNING METHOD PROVIDED")
+          self.goal_initialized = False
+        
+        #for i, p in enumerate(self.path):
+        #  self.make_rviz_marker(p, i, 1.0,0.0,0.0,0.5,10.0)
     
   def path_callback(self, path):  
     self.path_initialized = True
@@ -166,7 +182,10 @@ class PID_Controller(object):
         self.path_initialized = False
         rospy.logwarn("PID_Controller::Path Not updated")
     #rospy.logwarn("PID_Controller::control_timer_callback: in")
-    self.control_global()
+    if self.use_a_star_path:
+        self.control_global()
+    if self.use_naive_pid:
+        self.naive_control()
 
   def make_rviz_marker(self, p, i, r, g, b, s,d):
     m = Marker()
@@ -193,42 +212,16 @@ class PID_Controller(object):
     m.lifetime = rospy.Duration(d)
     self.pub_marker.publish(m)
 
-
-  def call_a_star_path_service(self, gx, gy):
-    if random.random() < 0.1:
-        return True
-    
-    try:
-      #rospy.logwarn("PID_Controller::call_a_star_path_service:: in: start: %.2f, %.2f", self.cLoc.x, self.cLoc.y)
-      #rospy.logwarn("PID_Controller::call_a_star_path_service:: in: goal: %.2f, %.2f", gx, gy)
-      resp = self.a_star_client(self.cLoc.x, self.cLoc.y, float(gx), float(gy), self.map_num)
-      #rospy.logwarn("call_a_star_path_service:: out with path len %i", len(resp.xs))
-      if resp.success:
-        self.path = []
-        #path_str = []
-        for i in range(0, len(resp.xs)):
-          # x,y,z,t,xs,ys,zs,ts
-          a = State(resp.xs[i], resp.ys[i], self.goal.z, 0.0, 0.0, 0.0, 0.0, 0.0);
-          self.path.append(a)
-          #path_str.append("[ " + str(resp.xs[i]) + ", " + str(resp.ys[i]) + " ], ")
-          
-        return True
-      else:
-        rospy.logwarn("call_a_star_path_service:: fail")
-        rospy.logwarn("   cLoc: %.2f, %.2f", self.cLoc.x, self.cLoc.y)
-        rospy.logwarn("   carrot: %.2f, %.2f", gx, gy)
-        self.path = []
-        return False
-    except:
-      rospy.logerr("PID Controller:: Failed to call A* service")
-
   def goal_callback(self, msg):
-    #rospy.logerr("PID_Controller::goal_callback: goal_in: %0.2f, %0.2f from loc %.2f, %.2f", msg.x,msg.y, self.cLoc.x, self.cLoc.y)
-    self.goal.x = msg.x
-    self.goal.y = msg.y
-    self.goal_initialized = True
-    self.make_rviz_marker(self.goal, 99, 0.0,0.0,1.0,1.5,10.0)
-    self.request_path()
+    #rospy.logwarn("PID_Controller::goal_callback: goal_in: %0.2f, %0.2f from loc %.2f, %.2f", msg.x,msg.y, self.cLoc.x, self.cLoc.y)
+    if abs(msg.x - self.goal.x) + abs(msg.y - self.goal.y) > 1.0:
+        #rospy.logwarn("PID_Controller::goal_callback: abs(%0.2f - %0.2f) + abs(%0.2f - %0.2f) = %0.2f)", msg.x, self.goal.x,msg.y, self.goal.y, abs(msg.y-self.goal.y) + abs(msg.x - self.goal.x))
+        self.goal.x = msg.x
+        self.goal.y = msg.y
+        self.goal_initialized = True
+        self.make_rviz_marker(self.goal, 99, 0.0,0.0,1.0,1.5,10.0)
+        #rospy.logwarn("PID Controller:: goal_callback: requesting path to %0.2f, %0.2f", self.goal.x, self.goal.y)
+        self.request_path()
 
   def odom_callback(self, msg):
     #rospy.logwarn("PID_Controller::odom_callback: in")
@@ -249,7 +242,7 @@ class PID_Controller(object):
       self.carrot.x = self.start.x
       self.carrot.y = self.start.y
       self.carrot.z = self.goal.z
-      rospy.logwarn("PID Controller::odom callback: initialized self")
+      rospy.loginfo("PID Controller::odom callback: initialized self")
       self.initialized = True
 
   def limit_goals(self):
@@ -270,19 +263,20 @@ class PID_Controller(object):
       self.carrot = self.goal
       return True
 
-    if len(self.path) > 1:
+    if self.path_initialized and len(self.path) > 1:
       #rospy.logerr("Path: %i", len(self.path))
       pp = self.path
       dd = math.sqrt((self.cLoc.x-self.goal.x)**2 + (self.cLoc.y-self.goal.y)**2)
       if dd < self.control_radius:
         # check if I am at my goal
-        self.carrot = self.goal
+        self.carrot.x = self.goal.x
+        self.carrot.y = self.goal.y
         return True
       for p in reversed(pp):
         # find the point along the path that maximizes the distance from me and is less than some distance
         dd = math.sqrt((self.cLoc.x-p.x)**2 + (self.cLoc.y-p.y)**2)
         #rospy.logerr("cloc: %.2f %.2f and pp: %.2f, %.2f with distance %.2f and control_radius %.2f", self.cLoc.x, self.cLoc.y, p.x, p.y, dd, self.control_radius)
-        if dd < self.control_radius:
+        if dd < self.carrot_distance:
           self.carrot.x = p.x
           self.carrot.y = p.y
           self.carrot.z = p.z
@@ -293,7 +287,8 @@ class PID_Controller(object):
       rospy.logerr("PID Controller::update carrot: could not update carrot")
       return False
     else:
-        rospy.logerr("PID Controller::update carrot: path is NULL")
+        self.path_initialized = False
+        #rospy.logerr("PID Controller::update carrot: path is NULL")
         return False
 
   def bearing_alignment(self, a, b):
@@ -345,7 +340,7 @@ class PID_Controller(object):
       ezs = self.carrot.zs - self.cLoc.zs
       pzs = self.pid_global_gains.zs * ezs + self.pid_global_gains.z * ez
       self.velocity_client(1,0.0,0.0, pzs, 0.0)
-      rospy.logwarn("PID_Controller::bad carrot")
+      #rospy.logwarn("PID_Controller::bad carrot")
       return
 
     self.in_loop = True
@@ -384,49 +379,43 @@ class PID_Controller(object):
     #print "******************** Carrot *******************************"
     #self.carrot.print_state()
 
-    err.xs = 0.0 - self.cLoc.xs
-    err.ys = 0.0 - self.cLoc.ys
-    err.zs = self.carrot.zs - self.cLoc.zs
-    err.ts = self.carrot.ts - self.cLoc.ts
+    err.xs = -self.cLoc.xs
+    err.ys = -self.cLoc.ys
+    err.zs = -self.cLoc.zs
+    err.ts = -self.cLoc.ts
 
     # put in some safety stuff
     # don't spin at goal
-    if abs(err.x) + abs(err.y) < self.goal_tolerance:
+    if abs(err.x) + abs(err.y) < self.control_radius:
       err.t = 0.0
       err.ts = 0.0
 
     #print "************************** error ************************************** "
     #err.print_state()
     
-    
-    #self.sum_err.x = min(10.0, max(self.sum_err.x + err.x,-10.0))
-    #self.sum_err.y = min(10.0, max(self.sum_err.y + err.y,-10.0))
-    #self.sum_err.z = min(10.0, max(self.sum_err.z + err.z,-10.0))
-    #self.sum_err.t = min(10.0, max(self.sum_err.t + err.t,-10.0))
-    #self.sum_err.xs = self.sum_err.xs + err.xs
-    #self.sum_err.ys = self.sum_err.ys + err.ys
-    #self.sum_err.zs = self.sum_err.zs + err.zs
-    #self.sum_err.ts = self.sum_err.ts + err.ts
-    
+    #print "************************** PID Gains ********************************** "
+    #self.pid_global_gains.print_state()
+       
     pid = State(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-    self.pid_global_gains.xs = 0.0
-    self.pid_global_gains.ys = 0.0
-    
-    pid.xs = self.pid_global_gains.xs * err.xs + self.pid_global_gains.x * err.x# + 0.0 * self.sum_err.x
-    pid.ys = self.pid_global_gains.xs * err.ys + self.pid_global_gains.x * err.y# + 0.0 * self.sum_err.y
-    pid.zs = self.pid_global_gains.zs * err.zs + self.pid_global_gains.z * err.z# + 0.0 * self.sum_err.z
-    pid.ts = 0.0*self.pid_global_gains.ts * err.ts + self.pid_global_gains.t * err.t# + 0.0 * self.sum_err.t
+    pid.xs = self.pid_global_gains.xs * err.xs + self.pid_global_gains.x * err.x
+    pid.ys = self.pid_global_gains.xs * err.ys + self.pid_global_gains.x * err.y
+    pid.zs = self.pid_global_gains.zs * err.zs + self.pid_global_gains.z * err.z
+    pid.ts = 0.0*self.pid_global_gains.ts * err.ts + self.pid_global_gains.t * err.t
 
     #print "***************************** pid-0 ***************************"
     #pid.print_state()
     
     # don't travel while at the wrong alt or pointing in the wrong direction
-    if abs(err.z) >= 1.0 or abs(err.t) >= pi/6.0:
+    if abs(err.z) >= 1.0:
       pid.xs = 0.0
       pid.ys = 0.0
-    elif abs(err.t) >= pi/12.0:
-      pid.xs += pid.xs * 0.5
-      pid.ys += pid.ys * 0.5
+    
+    if abs(err.t) >= pi/2.0:
+      pid.xs = 0.0
+      pid.ys = 0.0
+    else:
+      pid.xs *= (pi/2.0 - abs(err.t)) / (pi/2.0)
+      pid.ys *= (pi/2.0 - abs(err.t)) / (pi/2.0)
     
     #print "***************************** pid-1 ***************************"
     #pid.print_state()
@@ -459,7 +448,7 @@ class PID_Controller(object):
     #self.pub_twist.publish(twist) 
     self.in_loop = False
 
-  def at_goal(self):
+  def at_carrot(self):
     err = math.sqrt((self.carrot.x - self.cLoc.x)**2 + (self.carrot.y - self.cLoc.y)**2  + (self.carrot.z - self.cLoc.z)**2)
     error_yaw = abs(self.carrot.t - self.cLoc.t)
     if (err < 1.0 and error_yaw < 8.0):
